0d90e4ec02da084ec921a0841413581ef56317f1
Andrew Guenther <guenther.andrew.j@gmail.com>
2014-06-03 01:10:49 -0700

    Made events accept datetimes

--- arts/events.py
+++ arts/events.py
@@ -1,4 +1,20 @@
+import time
+
+
+TIME_FMT_STR = "%Y-%m-%dT%H:%M:%S"
+
+def _convert_time(time_val):
+    if type(time_val) is str:
+        return int(time.mktime(time.strptime(time_val, TIME_FMT_STR)))
+    elif type(time_val) is int:
+        return time_val
+    else:
+        raise ValueError("Unrecognized time format")
+
 def outage(data, start, end):
+    start = _convert_time(start)
+    end = _convert_time(end)
+
     for idx, (time, value) in enumerate(data):
         if time >= start and time <= end:
             data[idx] = (time, 0)
@@ -6,6 +22,9 @@ def outage(data, start, end):
     return data
 
 def spike(data, start, end, peak):
+    start = _convert_time(start)
+    end = _convert_time(end)
+
     mid = (start + end) / 2
     tail_size = mid - start
     for idx, (time, value) in enumerate(data):
